de4fb87fd67d53f0a300fc11776b47aa302b0c98
Rubén Sanchis <rusansor@gmail.com>
2016-04-20 00:01:42 +0200

    make add tweet metadata interactive

--- src/main/java/org/banking/twitter/commands/Commands.java
+++ src/main/java/org/banking/twitter/commands/Commands.java
@@ -52,41 +52,55 @@ public class Commands implements CommandMarker {
 	}
 
 	@CliCommand(value = "add-metadata", help = "Fetch and store tweets")
-	public String addMetadata(
-			@CliOption(key = { "tweetId" }, mandatory = false, help = "Tweet Id") final String tweetId)
+	public void addMetadata(
+			@CliOption(key = {
+					"loop" }, mandatory = true, help = "keep adding metadate to more tweets", specifiedDefaultValue = "true") final boolean loop)
 			throws JsonGenerationException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		if (tweetId == null) {
-			Tweet tweetWithoutMetadata = tweetRepository.findByMetadataIsNull();
-			String tweet = mapper.writeValueAsString(tweetWithoutMetadata);
-			logger.info(tweet);
-			ConsoleReader reader = new ConsoleReader();
-			String readLine = reader.readLine("Enter Category Definition[(A)cquisition, (E)ngagement, (R)etention]: ");
-			logger.info(readLine);
-			Metadata metadata = new Metadata();
-			if ("a".equals(readLine.toLowerCase())) {
-				metadata.setCategoryDefinition(CategoryDefinition.ACQUISITION);
-			} else if ("e".equals(readLine.toLowerCase())) {
-				metadata.setCategoryDefinition(CategoryDefinition.ENGAGEMENT);
-			} else if ("r".equals(readLine.toLowerCase())) {
-				metadata.setCategoryDefinition(CategoryDefinition.RETENTION);
-			} else {
-				System.out.println("not matching!!!!!!!!!!!!!!");
+		if (loop) {
+			while (true) {
+				Tweet tweet = tweetRepository.findByMetadataIsNull();
+				String userInput = readUserInput(tweet);
+				if ("q".equals(userInput.toLowerCase())) {
+					break;
+				}
+				addOneTweetMetadata(tweet, userInput);
 			}
-			tweetWithoutMetadata.setMetadata(metadata);
-			tweetRepository.save(tweetWithoutMetadata);
-			return "saved tweet with metadata";
 		} else {
-			Tweet tweet = tweetRepository.findOne(tweetId);
-			Metadata metadata = new Metadata();
-			metadata.setCategoryDefinition(CategoryDefinition.ENGAGEMENT);
-			tweet.setMetadata(metadata);
-			tweetRepository.save(tweet);
-
-			String tweetJson = mapper.writeValueAsString(tweet);
-			return "saved Tweet with content: " + tweetJson;
+			Tweet tweet = tweetRepository.findByMetadataIsNull();
+			String userInput = readUserInput(tweet);
+			if (!"q".equals(userInput.toLowerCase())) {
+				addOneTweetMetadata(tweet, userInput);
+			}
 		}
+	}
 
+	private void addOneTweetMetadata(Tweet tweet, String userInput) throws IOException {
+
+		Metadata metadata = new Metadata();
+		if ("a".equals(userInput.toLowerCase())) {
+			metadata.setCategoryDefinition(CategoryDefinition.ACQUISITION);
+		} else if ("e".equals(userInput.toLowerCase())) {
+			metadata.setCategoryDefinition(CategoryDefinition.ENGAGEMENT);
+		} else if ("r".equals(userInput.toLowerCase())) {
+			metadata.setCategoryDefinition(CategoryDefinition.RETENTION);
+		} else if ("q".equals(userInput.toLowerCase())) {
+
+		} else {
+			System.out.println("not matching!!!!!!!!!!!!!!");
+		}
+		tweet.setMetadata(metadata);
+		tweetRepository.save(tweet);
+		logger.info("saved tweet with metadata");
+	}
+
+	private String readUserInput(Tweet tweet) throws IOException {
+
+		logger.info(String.format("Id [%s], Text [%s]", tweet.getId(), tweet.getText()));
+		ConsoleReader reader = new ConsoleReader();
+		String readLine = reader.readLine(
+				"Enter Category Definition[(A)cquisition, (E)ngagement, (R)etention]    or (Q) to quit/stop editing tweets: ");
+		logger.info(readLine);
+		return readLine;
 	}
 
 }
